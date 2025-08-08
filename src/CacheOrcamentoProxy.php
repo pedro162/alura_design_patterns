6a176589a1247dc5734ada07011d7d91820d98ed
Jose Pedro Aguiar Ferreira <jose.ferreira@terceiros.solides.com.br>
2025-08-07 21:59:21 -0300

    Structural patterns

--- src/CacheOrcamentoProxy.php
+++ src/CacheOrcamentoProxy.php
@@ -2,26 +2,22 @@
 
 namespace Alura\DesignPattern;
 
-class CacheOrcamentoProxy extends Orcamento
+class CacheOrcamentoProxy implements Orcavel
 {
     private Orcamento $orcamento;
+    private ?float $valorCache = null;
 
     public function __construct(Orcamento $orcamento)
     {
         $this->orcamento = $orcamento;
     }
 
-    public function adicionarItem(Orcavel $item)
-    {
-        throw new \Exception('Não é possível adicionar itens a um orçamento cacheado.');
-    }
-
     public function getValor(): float
     {
-        if ($this->orcamento->valor === 0) {
-            $this->orcamento->valor = $this->orcamento->valor();
+        if ($this->valorCache === null) {
+            $this->valorCache = $this->orcamento->valor();
         }
 
-        return $this->orcamento->valor;
+        return $this->valorCache;
     }
 }
